e24466c6360b401d7ddee0d4f8a22b59593b04b1
42zz <m@42zz.net>
2018-09-03 08:44:21 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <h1>Vue Tree Browser</h1>
+    <TreeBrowser
+      :node="root"
+      @onClick="nodeWasClicked"
+    />
+  </div>
+</template>
+
+<script>
+import TreeBrowser from './components/TreeBrowser'
+import root from './root.json'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      root,
+    }
+  },
+  methods: {
+    nodeWasClicked(node) {
+      alert(node.name);
+    },
+  },
+  components: {
+    TreeBrowser
+  }
+}
+</script>
+
+<style>
+body {
+  background: #333;
+  color: #fff;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
